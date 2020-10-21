2efc1ff2d3d520b47ce9de89e0375cb152460e78
Ameya Nikose <ameynikose@gmail.com>
2020-10-21 09:59:08 +0530

    improved schematic map page

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-$(document).ready(function() {
-        $(window).scroll( function(){
-            $('.fadein').each( function(i){
-                var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-                var bottom_of_window = $(window).scrollTop() + $(window).height();
-                if( bottom_of_window > bottom_of_element ){
-                    $(this).animate({'opacity':'1'},1000);
-                }
-            });
-        });
-    });
-    
-
-
-
-
-
-
 
 //Smooth Scroll
 const scroll = new SmoothScroll('.nav-links, .arrow, #work, a[href*="#"]' ,{
