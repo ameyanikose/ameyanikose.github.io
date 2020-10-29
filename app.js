e7965a2e4012ba19891b4df6901938b2595bf5c6
Ameya Nikose <ameynikose@gmail.com>
2020-10-29 14:53:06 +0530

    solved intro-text bug

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 //Smooth Scroll
+
 const scroll = new SmoothScroll('.nav-links, .arrow, #work, a[href*="#"]', {
   speed: 900,
 });
 
 //Text Fade
-const text = document.querySelector(".intro-text");
-const strText = text.textContent;
-const splitText = strText.split("");
-text.textContent = "";
+
+// const text = document.querySelector(".intro-text");
+const text = "Hello!/I am a design apprentice/with interests in/experience design and/visual communication."
+// const strText = text.textContent;
+// const splitText = strText.split("");
+const splitText = text.split("");
+// text.textContent = "";
+
+
 for (let i = 0; i < splitText.length; i++) {
   if (splitText[i] == "/") {
-    text.innerHTML += "<br>";
+  //  text.innerHTML += "<br>";
+  document.getElementById("abcd").innerHTML += "<br>";
     continue;
   }
-  text.innerHTML += "<span>" + splitText[i] + "</span>";
+// text.innerHTML += "<span>" + splitText[i] + "</span>";
+document.getElementById("abcd").innerHTML += "<span>" + splitText[i] + "</span>";
 
   if (splitText[i] === " ") {
-    text.innerHTML += "<span>" + "&nbsp" + "</span>";
+    // text.innerHTML += "<span>" + "&nbsp" + "</span>";
+    document.getElementById("abcd").innerHTML += "<span>" + "&nbsp" + "</span>";
     continue;
   }
 }
@@ -25,7 +34,7 @@ let char = 0;
 let timer = setInterval(onTick, 15);
 
 function onTick() {
-  const span = text.querySelectorAll("span")[char];
+  const span = document.querySelectorAll("span")[char];
   span.classList.add("fade");
   char++;
   if (char > 97) {
@@ -39,12 +48,13 @@ function complete() {
   timer = null;
 }
 
+
+
 //Filter
 
-// $(document).ready(function () {
-  // alert(1)
+
   $(".proj-nav-link").click(function () {
-    // alert(2)
+    
     const name = $(this).attr("data-filter");
     if (name == "all") {
       $(".card-container").hide().fadeIn(500);
@@ -71,17 +81,17 @@ function complete() {
         .fadeIn(500);
     }
   });
-// });
+
 
 $(".proj-links a").click(function () {
   $(this).addClass("active").siblings().removeClass("active");
 });
 
-//Show more
-// $(document).ready(function () {
+
   $(".show-more").click(function () {
     $(".card-container-hidden").css("display", "block").hide().fadeIn(500);
     $(".show-more").hide();
     $(".work").css("padding-bottom", "10vh");
   });
-// });
+
+
